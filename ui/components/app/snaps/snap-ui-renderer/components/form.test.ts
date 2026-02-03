seaona/metamask-extension

commit aa3b15527af237ab4fd7c1ba912398e19ee172f1

chore(snaps): Bump Snaps packages (#38354)
## **Description**

This PR bumps the snaps related dependencies to the latest `v134.0.0`
release of the repo.

This includes:
- The new `DateTimePicker` component
- Forces the registry fetching without cache

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/38354?quickstart=1)

## **Changelog**

<!--
If this PR is not End-User-Facing and should not show up in the
CHANGELOG, you can choose to either:
1. Write `CHANGELOG entry: null`
2. Label with `no-changelog`

If this PR is End-User-Facing, please write a short User-Facing
description in the past tense like:
`CHANGELOG entry: Added a new tab for users to see their NFTs`
`CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`

(This helps the Release Engineer do their job more quickly and
accurately)
-->

CHANGELOG entry: Add `DateTimePicker` Snaps UI component and force
fetching the registry without cache

## **Related issues**

Fixes:

## **Manual testing steps**

1. Go to test-snaps
2. use the interactive-ui snap
3. select any date/time picker

## **Screenshots/Recordings**

<!-- If applicable, add screenshots and/or recordings to visualize the
before and after of your change. -->

### **Before**

<img width="411" height="632" alt="screenshot_27112025_145220"
src="https://github.com/user-attachments/assets/49e37143-93c8-4a7b-a290-399a28ac6f94"
/>
<img width="409" height="628" alt="screenshot_27112025_145123"
src="https://github.com/user-attachments/assets/a084f31d-196a-4d4c-b803-1b3268480e3b"
/>
<img width="411" height="633" alt="screenshot_27112025_145202"
src="https://github.com/user-attachments/assets/3592acff-10ea-4bc7-bc70-b92e0a26c0ac"
/>
<img width="408" height="632" alt="screenshot_27112025_145211"
src="https://github.com/user-attachments/assets/f9e8dc32-8b14-41f3-93db-ee096f708ac2"
/>


### **After**

<!-- [screenshots/recordings] -->

## **Pre-merge author checklist**

ui/contexts/snaps/snap-interface.tsx
@@ -104,9 +104,8 @@ export const SnapInterfaceContextProvider: FunctionComponent<
         params: {
           event: {
             type: event,
-            // TODO: Allow null in the types and simplify this
-            ...(name !== undefined && name !== null ? { name } : {}),
-            ...(value !== undefined && value !== null ? { value } : {}),
+            ...(name === undefined ? {} : { name }),
+            ...(value === undefined ? {} : { value }),
           },
           id: interfaceId,
         },
@@ -135,7 +134,7 @@ export const SnapInterfaceContextProvider: FunctionComponent<
   const handleEvent: HandleEvent = ({
     event,
     name,
-    value = name ? internalState.current[name] : undefined,
+    value = name ? internalState.current[name] : null,
   }) => handleSnapRequest(event, name, value);
 
   const submitInputChange = (name: string, value: State | null) =>

.yarnrc.yml
@@ -46,6 +46,7 @@ npmAuditIgnoreAdvisories:
   # Material UI dependencies are planned for removal
   - '@material-ui/core (deprecation)'
   - '@material-ui/styles (deprecation)'
+  - '@material-ui/pickers (deprecation)'
 
   # Dependencies brought in by @truffle/decoder that are deprecated:
   - 'cids (deprecation)' # via @ensdomains/content-hash

builds.yml
@@ -32,7 +32,7 @@ buildTypes:
       - ALLOW_LOCAL_SNAPS: false
       - REQUIRE_SNAPS_ALLOWLIST: true
       - REJECT_INVALID_SNAPS_PLATFORM_VERSION: true
-      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.2.3/index.html
+      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.3.0/index.html
       - ACCOUNT_SNAPS_DIRECTORY_URL: https://snaps.metamask.io/account-management
       - IS_SIDEPANEL: true
       - EXTENSION_UX_PNA25: true
@@ -67,7 +67,7 @@ buildTypes:
       - ALLOW_LOCAL_SNAPS: false
       - REQUIRE_SNAPS_ALLOWLIST: true
       - REJECT_INVALID_SNAPS_PLATFORM_VERSION: true
-      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.2.3/index.html
+      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.3.0/index.html
       - ACCOUNT_SNAPS_DIRECTORY_URL: https://snaps.metamask.io/account-management
       - IS_SIDEPANEL: true
       - EXTENSION_UX_PNA25: true
@@ -103,7 +103,7 @@ buildTypes:
       - IS_SIDEPANEL: true
       - REQUIRE_SNAPS_ALLOWLIST: true
       - REJECT_INVALID_SNAPS_PLATFORM_VERSION: true
-      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.2.3/index.html
+      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.3.0/index.html
       - ACCOUNT_SNAPS_DIRECTORY_URL: https://snaps.metamask.io/account-management
       - EXTENSION_UX_PNA25: true
       # for seedless onboarding (social login)
@@ -133,7 +133,7 @@ buildTypes:
       - ALLOW_LOCAL_SNAPS: true
       - REQUIRE_SNAPS_ALLOWLIST: false
       - REJECT_INVALID_SNAPS_PLATFORM_VERSION: false
-      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.2.3/index.html
+      - IFRAME_EXECUTION_ENVIRONMENT_URL: https://execution.metamask.io/iframe/10.3.0/index.html
       - SUPPORT_LINK: https://support.metamask.io/
       - SUPPORT_REQUEST_LINK: https://support.metamask.io/
       - INFURA_ENV_KEY_REF: INFURA_FLASK_PROJECT_ID

test/e2e/page-objects/pages/dialog/snap-interactive-dialog.ts
@@ -1,4 +1,5 @@
 import { strict as assert } from 'assert';
+import { DateTime } from 'luxon';
 import { Driver } from '../../../webdriver/driver';
 
 const selectors = {
@@ -12,6 +13,15 @@ const selectors = {
   selectorItem: '.snap-ui-renderer__selector-item',
   rendererPanel: '.snap-ui-renderer__panel',
   exampleCheckbox: '.mm-checkbox__input',
+  exampleDateTimePicker: '.snap-ui-renderer__date-time-picker--datetime',
+  exampleDatePicker: '.snap-ui-renderer__date-time-picker--date',
+  exampleTimePicker: '.snap-ui-renderer__date-time-picker--time',
+  datePickerCalendarContainer: '.MuiPickersSlideTransition-transitionContainer',
+  datePickerPreviousMonthButton: '.MuiPickersCalendarHeader-iconButton',
+  datePickerDayButton: '.MuiPickersDay-day',
+  timePickerHourButton: '.MuiPickersClockNumber-clockNumber',
+  dateTimePickerOkButton:
+    '.MuiPickersModal-withAdditionalAction > button:nth-child(3)',
 } satisfies Record<string, string | Record<string, string>>;
 
 class SnapInteractiveDialog {
@@ -63,6 +73,144 @@ class SnapInteractiveDialog {
     await this.driver.fill(selectors.exampleInput, message);
   }
 
+  /**
+   * Open, select a date and time and submit the values in the date-time picker of the Snap Interactive Dialog.
+   *
+   * @param day - The day of the month to select.
+   * @param hour - The hour to select.
+   * @param minute - The minute to select.
+   * @returns The ISO string representation of the selected date and time.
+   */
+  async selectInDateTimePicker(day: number, hour: number, minute: number) {
+    await this.driver.clickElement(selectors.exampleDateTimePicker);
+
+    await this.#selectDateInPicker(day);
+    await this.#selectTimeInPicker(hour, minute);
+    await this.driver.clickElement(selectors.dateTimePickerOkButton);
+
+    return (
+      DateTime.now()
+        // We select the previous month in the picker to ensure consistent tests.
+        // Since we use `disableFuture` in the picker, if we select a day that is
+        // greater than today when running the test at the beginning of the month,
+        // the date won't be selectable.
+        .minus({ months: 1 })
+        .set({
+          day,
+          hour,
+          minute,
+          second: 0,
+          millisecond: 0,
+        })
+        .toISO()
+    );
+  }
+
+  /**
+   * Open, select a time and submit the values in the time picker of the Snap Interactive Dialog.
+   *
+   * @param hour - The hour to select.
+   * @param minute - The minute to select.
+   * @returns The ISO string representation of the selected time.
+   */
+  async selectInTimePicker(hour: number, minute: number) {
+    await this.driver.clickElement(selectors.exampleTimePicker);
+
+    await this.#selectTimeInPicker(hour, minute);
+
+    await this.driver.clickElement(selectors.dateTimePickerOkButton);
+
+    return DateTime.now()
+      .set({
+        hour,
+        minute,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISO();
+  }
+
+  /**
+   * Open, select a date and submit the value in the date picker of the Snap Interactive Dialog.
+   *
+   * @param day - The day of the month to select.
+   * @returns The ISO string representation of the selected date.
+   */
+  async selectInDatePicker(day: number) {
+    await this.driver.clickElement(selectors.exampleDatePicker);
+
+    await this.#selectDateInPicker(day);
+
+    await this.driver.clickElement(selectors.dateTimePickerOkButton);
+
+    return (
+      DateTime.now()
+        // We select the previous month in the picker to ensure consistent tests.
+        // Since we use `disableFuture` in the picker, if we select a day that is
+        // greater than today when running the test at the beginning of the month,
+        // the date won't be selectable.
+        .minus({ months: 1 })
+        .set({
+          day,
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toISO()
+    );
+  }
+
+  /**
+   * Select a date in the calendar of a `DateTimePicker` component.
+   *
+   * @param day - The day of the month to select.
+   */
+  async #selectDateInPicker(day: number) {
+    await this.driver.clickElement(selectors.datePickerPreviousMonthButton);
+    await this.driver.waitForElementToStopMoving(
+      selectors.datePickerCalendarContainer,
+    );
+
+    await this.driver.clickElement({
+      xpath: `//button[span[p[contains(text(), "${day}")]]]`,
+    });
+  }
+
+  /**
+   * Select a time in the clock of a `DateTimePicker` component.
+   *
+   * @param hours - The hour to select.
+   * @param minutes - The minute to select.
+   */
+  async #selectTimeInPicker(hours: number, minutes: number) {
+    console.log(`Selecting time in picker: ${hours}:${minutes}`);
+
+    // We need to use actions here because the hour and minute selection is made
+    // by dragging the clock hand, or clicking the mask behind the numbers.
+    await this.driver.clickPoint(
+      {
+        text: `${hours}`,
+        tag: 'span',
+        css: selectors.timePickerHourButton,
+      },
+      1,
+      1,
+    );
+
+    // We need to use actions here because the hour and minute selection is made
+    // by dragging the clock hand, or clicking the mask behind the numbers.
+    await this.driver.clickPoint(
+      {
+        text: `${minutes}`,
+        tag: 'span',
+        css: selectors.timePickerHourButton,
+      },
+      1,
+      1,
+    );
+  }
+
   async selectDropDownOption(exampleDropName: string, option: string) {
     console.log(`Selecting selector option: "${option}"`);
     if (exampleDropName === 'selector') {
@@ -94,7 +242,15 @@ class SnapInteractiveDialog {
     await this.driver.clickElement(selectors.exampleCheckbox);
   }
 
-  async checkResult() {
+  async checkResult({
+    dateTimePickerDate,
+    datePickerDate,
+    timePickerDate,
+  }: {
+    dateTimePickerDate: string;
+    datePickerDate: string;
+    timePickerDate: string;
+  }) {
     console.log(`Checking that all the selected options appear in the result`);
     await this.driver.waitForSelector({
       text: 'foo bar',
@@ -114,6 +270,19 @@ class SnapInteractiveDialog {
     });
     await this.driver.waitForSelector({
       text: 'true',
+      css: selectors.rendererPanel,
+    });
+
+    await this.driver.waitForSelector({
+      text: dateTimePickerDate,
+      css: selectors.rendererPanel,
+    });
+    await this.driver.waitForSelector({
+      text: datePickerDate,
+      css: selectors.rendererPanel,
+    });
+    await this.driver.waitForSelector({
+      text: timePickerDate,
       css: selectors.rendererPanel,
     });
   }

test/e2e/snaps/test-snap-interactive-ui.spec.ts
@@ -47,10 +47,22 @@ describe('Interactive UI Snap', function () {
         await interactiveUI.selectRadioOption('Option 1');
         await interactiveUI.selectDropDownOption('dropDown', 'Option 2');
         await interactiveUI.selectCheckbox();
+
+        const dateTimePickerDate = await interactiveUI.selectInDateTimePicker(
+          15,
+          6,
+          30,
+        );
+        const datePickerDate = await interactiveUI.selectInDatePicker(20);
+        const timePickerDate = await interactiveUI.selectInTimePicker(9, 40);
         await interactiveUI.clickSubmitButton();
 
         // check for returned values and close the dialog
-        await interactiveUI.checkResult();
+        await interactiveUI.checkResult({
+          dateTimePickerDate,
+          datePickerDate,
+          timePickerDate,
+        });
         await interactiveUI.clickOKButton();
         await driver.switchToWindowWithTitle(WINDOW_TITLES.TestSnaps);
         await testSnaps.checkMessageResultSpan(

ui/components/app/metamask-template-renderer/safe-component-list.js
@@ -51,6 +51,7 @@ import { SnapUISelector } from '../snaps/snap-ui-selector';
 import { SnapUITooltip } from '../snaps/snap-ui-tooltip';
 import { SnapUIAssetSelector } from '../snaps/snap-ui-asset-selector';
 import { SnapUIAccountSelector } from '../snaps/snap-ui-account-selector';
+import { SnapUIDateTimePicker } from '../snaps/snap-ui-date-time-picker';
 ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
 import { SnapAccountErrorMessage } from '../../../pages/confirmations/components/snap-account-error-message';
 import { SnapAccountSuccessMessage } from '../../../pages/confirmations/components/snap-account-success-message';
@@ -113,6 +114,7 @@ export const safeComponentList = {
   SnapUISelector,
   SnapUITooltip,
   SnapUIAssetSelector,
+  SnapUIDateTimePicker,
   span: 'span',
   Spinner,
   Skeleton,

ui/components/app/snaps/snap-ui-date-time-picker/index.ts
@@ -1,0 +1,1 @@
+export * from './snap-ui-date-time-picker';

ui/components/app/snaps/snap-ui-date-time-picker/snap-ui-date-time-picker.tsx
@@ -1,0 +1,203 @@
+import React, { FunctionComponent, useEffect } from 'react';
+import { DatePicker, DateTimePicker, TimePicker } from '@material-ui/pickers';
+import { Box } from '@metamask/design-system-react';
+import classnames from 'classnames';
+import { DateTime } from 'luxon';
+import { makeStyles } from '@material-ui/core/styles';
+import { HelpText, HelpTextSeverity, Label } from '../../../component-library';
+import { useSnapInterfaceContext } from '../../../../contexts/snaps';
+import { useI18nContext } from '../../../../hooks/useI18nContext';
+
+/**
+ * The props for the SnapUIDateTimePicker component.
+ */
+export type SnapUIDateTimePickerProps = {
+  name: string;
+  type: 'date' | 'time' | 'datetime';
+  label?: string;
+  error?: string;
+  placeholder?: string;
+  form?: string;
+  disablePast?: boolean;
+  disableFuture?: boolean;
+  disabled?: boolean;
+};
+
+/**
+ * Styles for the SnapUIDateTimePicker component.
+ */
+const useStyle = makeStyles({
+  root: {
+    width: '100%',
+  },
+  input: {
+    fontFamily: 'var(--font-family-default)',
+    backgroundColor: 'var(--color-background-default)',
+    border: '1px solid var(--color-border-muted)',
+    color: 'var(--color-text-default)',
+    height: '100%',
+    maxHeight: '58px',
+    minHeight: '48px',
+    display: 'inline-flex',
+    alignItems: 'center',
+    borderRadius: '8px',
+    fontSize: 'var(--typography-s-body-md-font-size)',
+    '& > input': {
+      padding: '0 16px',
+    },
+  },
+});
+
+/**
+ * Normalizes the date based on the picker type.
+ *
+ * @param date - The date to normalize.
+ * @param type - The type of the picker (date, time, datetime).
+ * @returns The normalized date.
+ */
+function normalizeDate(
+  date: DateTime | null,
+  type: 'date' | 'time' | 'datetime',
+): DateTime | null {
+  if (!date) {
+    return null;
+  }
+  switch (type) {
+    case 'date':
+      return date.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+    case 'time':
+      return date.set({ second: 0, millisecond: 0 });
+    case 'datetime':
+    default:
+      return date;
+  }
+}
+
+/**
+ * The SnapUIDateTimePicker component.
+ *
+ * @param props - The component props.
+ * @param props.name - The name of the input.
+ * @param props.type - The type of the picker (date, time, datetime).
+ * @param props.label - The label for the picker.
+ * @param props.form - The form identifier.
+ * @param props.disabled - Whether the picker is disabled.
+ * @param props.error - The error message to display.
+ * @param props.disablePast - Whether to disable past dates (only for date and datetime types).
+ * @param props.disableFuture - Whether to disable future dates (only for date and datetime types).
+ * @param props.placeholder - The placeholder text for the picker.
+ * @returns The DateTimePicker component.
+ */
+export const SnapUIDateTimePicker: FunctionComponent<
+  SnapUIDateTimePickerProps
+> = ({
+  type = 'datetime',
+  label,
+  placeholder,
+  name,
+  form,
+  disabled,
+  error,
+  disablePast = false,
+  disableFuture = false,
+}) => {
+  const t = useI18nContext();
+  const { handleInputChange, getValue } = useSnapInterfaceContext();
+
+  const initialValue = getValue(name, form) as string;
+
+  const [value, setValue] = React.useState<DateTime | null>(
+    initialValue ? DateTime.fromISO(initialValue) : null,
+  );
+
+  const classes = useStyle();
+
+  useEffect(() => {
+    if (initialValue !== undefined && initialValue !== null) {
+      setValue(DateTime.fromISO(initialValue));
+    }
+  }, [initialValue]);
+
+  const handleChange = (date: DateTime | null) => {
+    const normalizedDate = normalizeDate(date, type);
+
+    const isoString = normalizedDate ? normalizedDate.toISO() : null;
+
+    setValue(normalizedDate);
+    handleInputChange(name, isoString, form);
+  };
+
+  return (
+    <Box
+      className={classnames('snap-ui-renderer__date-time-picker', {
+        'snap-ui-renderer__field': label !== undefined,
+      })}
+    >
+      {label && <Label htmlFor={name}>{label}</Label>}
+      {type === 'datetime' && (
+        <DateTimePicker
+          className={classnames(
+            classes.root,
+            'snap-ui-renderer__date-time-picker--datetime',
+          )}
+          value={value}
+          onChange={handleChange}
+          disabled={disabled}
+          disablePast={disablePast}
+          disableFuture={disableFuture}
+          placeholder={placeholder}
+          clearable
+          InputProps={{ disableUnderline: true, className: classes.input }}
+          format={'D T'}
+          clearLabel={t('clear')}
+          cancelLabel={t('cancel')}
+          okLabel={t('ok').toUpperCase()}
+          ampm={false}
+        />
+      )}
+      {type === 'date' && (
+        <DatePicker
+          className={classnames(
+            classes.root,
+            'snap-ui-renderer__date-time-picker--date',
+          )}
+          value={value}
+          onChange={handleChange}
+          disabled={disabled}
+          disablePast={disablePast}
+          disableFuture={disableFuture}
+          InputProps={{ disableUnderline: true, className: classes.input }}
+          clearable
+          placeholder={placeholder}
+          format={'D'}
+          clearLabel={t('clear')}
+          cancelLabel={t('cancel')}
+          okLabel={t('ok').toUpperCase()}
+        />
+      )}
+      {type === 'time' && (
+        <TimePicker
+          className={classnames(
+            classes.root,
+            'snap-ui-renderer__date-time-picker--time',
+          )}
+          value={value}
+          onChange={handleChange}
+          disabled={disabled}
+          InputProps={{ disableUnderline: true, className: classes.input }}
+          clearable
+          placeholder={placeholder}
+          clearLabel={t('clear')}
+          cancelLabel={t('cancel')}
+          okLabel={t('ok').toUpperCase()}
+          ampm={false}
+        />
+      )}
+      {error && (
+        <HelpText severity={HelpTextSeverity.Danger} marginTop={1}>
+          {error}
+        </HelpText>
+      )}
+    </Box>
+  );
+};

commit 094fa7fdf6931ee7785e0bb7e67b38b9e6251509

fix: Prevent Snap crashing when clicking buttons without names cp-13.17.0 (#39727)
<!--
Please submit this PR as a draft initially.
Do not mark it as "Ready for review" until the template has been
completely filled out, and PR status checks have passed at least once.
-->

## **Description**

Fixes an issue introduced in
https://github.com/MetaMask/metamask-extension/commit/aa3b15527af237ab4fd7c1ba912398e19ee172f1
that caused Snaps to crash when using buttons without names. We changed
the fallback for `value` to `null` which gets passed as a parameter to
`onUserInput`. This is invalid and crashes the Snap.

[![Open in GitHub
Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/MetaMask/metamask-extension/pull/39727?quickstart=1)

## **Changelog**

<!--
If this PR is not End-User-Facing and should not show up in the
CHANGELOG, you can choose to either:
1. Write `CHANGELOG entry: null`
2. Label with `no-changelog`

If this PR is End-User-Facing, please write a short User-Facing
description in the past tense like:
`CHANGELOG entry: Added a new tab for users to see their NFTs`
`CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`

(This helps the Release Engineer do their job more quickly and
accurately)
-->

CHANGELOG entry: Prevent Snap crashing when clicking buttons without
names

## **Related issues**

https://consensyssoftware.atlassian.net/browse/WPC-362

## **Manual testing steps**

1. Install the following Snap:
https://snaps.metamask.io/snap/npm/quai-snap/
2. Go to its home page
3. Click send
4. Fill out the form with some bogus values (your own address and some
value)
5. Click send
6. Check that the Snap does not crash in the console

<!-- CURSOR_SUMMARY -->
---

> [!NOTE]
> **Low Risk**
> Low risk, targeted change to Snap `onUserInput` event payloads; main
risk is subtle behavior change if any Snap relied on a `value` field for
`ButtonClickEvent`.
> 
> **Overview**

ui/contexts/snaps/snap-interface.tsx
@@ -105,7 +105,11 @@ export const SnapInterfaceContextProvider: FunctionComponent<
           event: {
             type: event,
             ...(name === undefined ? {} : { name }),
-            ...(value === undefined ? {} : { value }),
+            // Ensure `value` is always stripped for button clicks as buttons do not have a value (null is also disallowed).
+            ...(event === UserInputEventType.ButtonClickEvent ||
+            value === undefined
+              ? {}
+              : { value }),
           },
           id: interfaceId,
         },

ui/components/app/snaps/snap-ui-renderer/components/form.test.ts
@@ -251,4 +251,78 @@ describe('SnapUIForm', () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  it('submits correctly when button has no name', () => {
+    const { getByRole } = renderInterface(
+      Box({
+        children: Form({
+          name: 'form',
+          children: [
+            Field({ label: 'My Input', children: Input({ name: 'input' }) }),
+            Field({
+              label: 'Checkbox',
+              children: Checkbox({ name: 'checkbox' }),
+            }),
+            Button({ type: 'submit', children: 'Submit' }),
+          ],
+        }),
+      }),
+    );
+
+    const input = getByRole('textbox');
+    fireEvent.change(input, { target: { value: 'abc' } });
+
+    const checkbox = getByRole('checkbox');
+    fireEvent.click(checkbox);
+
+    const button = getByRole('button');
+    fireEvent.click(button);
+
+    expect(submitRequestToBackground).toHaveBeenNthCalledWith(
+      5,
+      'handleSnapRequest',
+      [
+        {
+          handler: 'onUserInput',
+          origin: 'metamask',
+          request: {
+            jsonrpc: '2.0',
+            method: ' ',
+            params: {
+              event: { type: 'ButtonClickEvent' },
+              id: MOCK_INTERFACE_ID,
+            },
+          },
+          snapId: MOCK_SNAP_ID,
+        },
+      ],
+    );
+
+    expect(submitRequestToBackground).toHaveBeenNthCalledWith(
+      6,
+      'handleSnapRequest',
+      [
+        {
+          handler: 'onUserInput',
+          origin: 'metamask',
+          request: {
+            jsonrpc: '2.0',
+            method: ' ',
+            params: {
+              event: {
+                name: 'form',
+                type: 'FormSubmitEvent',
+                value: {
+                  checkbox: true,
+                  input: 'abc',
+                },
+              },
+              id: MOCK_INTERFACE_ID,
+            },
+          },
+          snapId: MOCK_SNAP_ID,
+        },
+      ],
+    );
+  });
 });